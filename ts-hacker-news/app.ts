26d208121b1df334c61a975f46d524da727a77b9
xdswwwj <xdswwwj@gmail.com>
2022-08-10 00:43:49 +0900

    타입스크립트를 위한 환경 설정

---
+++ ts-hacker-news/app.ts
@@ -0,0 +1,186 @@
+const ajax = new XMLHttpRequest();
+const container = document.getElementById("root");
+const content = document.createElement("div");
+const NEWS_URL = "https://api.hnpwa.com/v0/news/@page.json";
+const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+const store = {
+  currentPage: 1,
+  feeds: [],
+};
+
+// NOTE: get 요청 xhr 함수
+function getData(url: string) {
+  ajax.open("GET", url, false);
+  ajax.send();
+
+  return JSON.parse(ajax.response);
+}
+
+function makeFeeds(feeds) {
+  for (let i = 0; i < feeds.length; i++) {
+    feeds[i].read = false;
+  }
+
+  return feeds;
+}
+
+// NOTE: news 리스트
+function newsFeed() {
+  let newsFeed = store.feeds;
+  const newsList = [];
+  const newsListLength = newsFeed.length;
+  const showList = 10;
+  const maxPage = newsListLength / showList;
+
+  let template = `
+    <div class="bg-gray-600 min-h-screen">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+            <div class="flex justify-start">
+              <h1 class="font-extrabold">Hacker News</h1>
+            </div>
+            <div class="items-center justify-end">
+              <a href="#/page/{{__prev_page__}}" class="text-gray-500">Previous</a>
+              <a href="#/page/{{__next_page__}}" class="text-gray-500 ml-4">Next</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 text-2xl text-gray-700">{{__news_feed__}}</div>
+    </div>
+
+    `;
+
+  if (newsFeed.length === 0) {
+    newsFeed = store.feeds = makeFeeds(
+      getData(NEWS_URL.replace("@page", store.currentPage))
+    );
+  }
+
+  for (
+    let i = (store.currentPage - 1) * showList;
+    i < store.currentPage * showList;
+    i++
+  ) {
+    const item = newsFeed[i];
+    newsList.push(`
+      <div class="p-6 ${
+        item.read ? "bg-red-500" : "bg-white"
+      } mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+        <div class="flex">
+          <div class="flex-auto"><a href="#/show/${item.id}">${
+      item.title
+    }</a></div>
+          <div class="text-center text-sm">
+            <div class="w-10 text-while bg-green-300 rounded-lg px-0 py-2">
+              ${item.comments_count}
+            </div>
+          </div>
+        </div>
+        <div class="flex mt-3">
+          <div class="grid grid-cols-3 text-sm text-gray-500">
+            <div><i class="fas fa-user mr-1"></i>${item.user}</div>
+            <div><i class="fas fa-heart mr-1"></i>${item.points}</div>
+            <div><i class="fas fa-clock mr-1"></i>${item.time_ago}</div>
+          </div>
+        </div>
+      </div>
+    `);
+  }
+
+  template = template.replace("{{__news_feed__}}", newsList.join(""));
+  template = template.replace(
+    "{{__prev_page__}}",
+    store.currentPage > 1 ? store.currentPage - 1 : 1
+  );
+  template = template.replace(
+    "{{__next_page__}}",
+    store.currentPage < maxPage ? store.currentPage + 1 : maxPage
+  );
+
+  container.innerHTML = template;
+}
+
+// NOTE: news 디테일
+function newsDetail() {
+  const id = location.hash.substr(7);
+  const newsContent = getData(CONTENT_URL.replace("@id", id));
+  let template = `
+  <div class="bg-gray-600 min-h-screen pb-8">
+    <div class="bg-white text-xl">
+      <div class="mx-auto px-4">
+        <div class="flex justify-between items-center py-6">
+          <div class="flex justify-start">
+            <div class="h1 font-extrabold">Hacker News</div>
+          </div>
+          <div class="items-center justify-end">
+            <a href="#/page/${store.currentPage}" class="text-gray-500">
+              <i class="fa fa-times"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="h-full border rounded-xl bg-white m-6 p-4">
+      <h2>${newsContent.title}</h2>
+      <div class="text-gray-400 h-20">${newsContent.content}</div>
+      {{__comments__}}
+    </div>
+  </div>
+  `;
+
+  for (let i = 0; i < store.feeds.length; i++) {
+    if (store.feeds[i].id === Number(id)) {
+      store.feeds[i].read = true;
+      break;
+    }
+  }
+
+  function makeComment(comments, called = 0) {
+    let commentString = [];
+
+    for (let i = 0; i < comments.length; i++) {
+      const item = comments[i];
+      commentString.push(`
+        <div style="padding-left: ${called * 40}px;" class="mt-4">
+          <div class="text-gray-400">
+            <i class="fa fa-sort-up mr-2"></i>
+            <strong>${item.user}</strong> ${item.time_ago}
+          </div>
+          <p className="text-gray-700">${item.content}</p>
+        </div>
+      `);
+
+      if (item.comments.length > 0) {
+        commentString.push(makeComment(item.comments, called + 1));
+      }
+    }
+
+    return commentString.join("");
+  }
+
+  container.innerHTML = template.replace(
+    "{{__comments__}}",
+    makeComment(newsContent.comments)
+  );
+}
+
+// NOTE: router 함수
+function router() {
+  const routePath = location.hash;
+  console.log(routePath);
+  if (routePath === "") {
+    newsFeed();
+  } else if (routePath.indexOf("#/page/") >= 0) {
+    store.currentPage = Number(routePath.substr(7));
+    newsFeed();
+  } else {
+    console.log("newsDetail");
+    newsDetail();
+  }
+}
+
+window.addEventListener("hashchange", router);
+
+router();
