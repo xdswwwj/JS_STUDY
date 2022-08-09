6a8ac26aa9050247dfec1082a9689669580ea3b0
xdswwwj <xdswwwj@gmail.com>
2022-08-09 23:54:54 +0900

    상태를 가져보자, 읽은 글 표시하기

--- hacker-news/app.js
+++ hacker-news/app.js
@@ -5,6 +5,7 @@ const NEWS_URL = "https://api.hnpwa.com/v0/news/@page.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 const store = {
   currentPage: 1,
+  feeds: [],
 };
 
 // NOTE: get 요청 xhr 함수
@@ -15,33 +16,47 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
+function makeFeeds(feeds) {
+  for (let i = 0; i < feeds.length; i++) {
+    feeds[i].read = false;
+  }
+
+  return feeds;
+}
+
 // NOTE: news 리스트
 function newsFeed() {
+  let newsFeed = store.feeds;
   const newsList = [];
-  const newsFeed = getData(NEWS_URL.replace("@page", store.currentPage));
   const newsListLength = newsFeed.length;
   const showList = 10;
   const maxPage = newsListLength / showList;
 
   let template = `
-  <div class="bg-gray-600 min-h-screen">
-    <div class="bg-white text-xl">
-      <div class="mx-auto px-4">
-        <div class="flex justify-between items-center py-6">
-          <div class="flex justify-start">
-            <h1 class="font-extrabold">Hacker News</h1>
-          </div>
-          <div class="items-center justify-end">
-            <a href="#/page/{{__prev_page__}}" class="text-gray-500">Previous</a>
-            <a href="#/page/{{__next_page__}}" class="text-gray-500 ml-4">Next</a>
+    <div class="bg-gray-600 min-h-screen">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+            <div class="flex justify-start">
+              <h1 class="font-extrabold">Hacker News</h1>
+            </div>
+            <div class="items-center justify-end">
+              <a href="#/page/{{__prev_page__}}" class="text-gray-500">Previous</a>
+              <a href="#/page/{{__next_page__}}" class="text-gray-500 ml-4">Next</a>
+            </div>
           </div>
         </div>
       </div>
+      <div class="p-4 text-2xl text-gray-700">{{__news_feed__}}</div>
     </div>
-    <div class="p-4 text-2xl text-gray-700">{{__news_feed__}}</div>
-  </div>
 
-  `;
+    `;
+
+  if (newsFeed.length === 0) {
+    newsFeed = store.feeds = makeFeeds(
+      getData(NEWS_URL.replace("@page", store.currentPage))
+    );
+  }
 
   for (
     let i = (store.currentPage - 1) * showList;
@@ -50,9 +65,13 @@ function newsFeed() {
   ) {
     const item = newsFeed[i];
     newsList.push(`
-      <div class="p-6 bg-white mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+      <div class="p-6 ${
+        item.read ? "bg-red-500" : "bg-white"
+      } mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
         <div class="flex">
-          <div class="flex-auto"><a href="#/show/${item.id}">${item.title}</a></div>
+          <div class="flex-auto"><a href="#/show/${item.id}">${
+      item.title
+    }</a></div>
           <div class="text-center text-sm">
             <div class="w-10 text-while bg-green-300 rounded-lg px-0 py-2">
               ${item.comments_count}
@@ -110,6 +129,13 @@ function newsDetail() {
     </div>
   </div>
   `;
+
+  for (let i = 0; i < store.feeds.length; i++) {
+    if (store.feeds[i].id === Number(id)) {
+      store.feeds[i].read = true;
+      break;
+    }
+  }
 
   function makeComment(comments, called = 0) {
     let commentString = [];
